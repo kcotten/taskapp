a47d27dab89b3ebb5440f54f66bfa470ed3b00a7
kcotten <kris.cotten@gmail.com>
2019-10-10 13:15:28 -0700

    Init remote

---
+++ app/static/scripts.js
@@ -0,0 +1,9 @@
+// check if vars are available
+var user_tasks = [];
+function taskCollector(tasks) {
+    console.log("hello");
+    console.log(tasks);
+    user_tasks = tasks;
+}
+
+console.log(user_tasks);
