65a88f6c1eca5f4bcc0dc507881202da6b182e55
kcotten <kris.cotten@gmail.com>
2019-10-10 13:19:30 -0700

    Merge changes from desktop

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,9 +1,30 @@
 // check if vars are available
 var user_tasks = [];
 function taskCollector(tasks) {
-    console.log("hello");
-    console.log(tasks);
     user_tasks = tasks;
 }
 
 console.log(user_tasks);
+
+var app = function() {
+    self.initTracks = function() {
+        this.console.log(tasks);
+    }
+
+    self.vue = new Vue({
+        el: "#vue-div",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['{{','}}'],
+        data: {
+            tasks: user_tasks
+        },
+        mounted() {
+            this.initTracks();
+        },
+        methods: {
+            initTracks: self.initTracks,
+        }
+    });
+
+    return self;
+}
